909fd915709b789d93c7457d90960ee93d9f203e
捕风的逍遥侯 <aohanhongzhi@qq.com>
2024-01-29 14:14:21 +0800

    bump javalin from 5.6.3 to 6.0.0

--- app/src/main/kotlin/hxy/dragon/Main.kt
+++ app/src/main/kotlin/hxy/dragon/Main.kt
@@ -21,19 +21,52 @@ private val log = KotlinLogging.logger {}
 fun main() {
 
     val app = Javalin.create { config ->
+        config.useVirtualThreads = true
         config.http.defaultContentType = "text/plain; charset=utf-8" // 解决 ctx#result 返回中文乱码。
-//        config.routing.ignoreTrailingSlashes = true // treat '/path' and '/path/' as the same path 默认就是支持尾斜杠
+        config.router.ignoreTrailingSlashes = true // treat '/path' and '/path/' as the same path 默认就是支持尾斜杠
         // treat '/path//subpath' and '/path/subpath' as the same path
-        config.routing.treatMultipleSlashesAsSingleSlash = true
+        config.router.treatMultipleSlashesAsSingleSlash = true
         config.jsonMapper(JavalinJackson().updateMapper { mapper ->
 //             如果是字段值是null，那么就序列化直接忽略
             mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL)
         })
-        config.plugins.enableDevLogging() // 调试请求日志
-        config.plugins.enableRouteOverview("routes") // 访问 http://localhost:7000/routes
-    }.start(7000)
+        config.bundledPlugins.enableDevLogging() // 调试请求日志
+        config.bundledPlugins.enableRouteOverview("routes") // 访问 http://localhost:7000/routes
 
-    routes(app)
+        config.router.apiBuilder {
+
+            get("/") { ctx ->
+                log.info { "首页访问中" }
+                ctx.result("Hello World，你好世界！—— Javalin6 for java & kotlin")
+            }
+            get("/param", IndexController::paramReceive)
+            post("/user", IndexController::bodyReceive)
+
+
+            // 组合式 Handler groups https://javalin.io/documentation#handler-groups
+            path("customer") {
+                post(CustomerController::createCustomer)
+                delete(CustomerController::deleteCustomer)
+                put(CustomerController::updateCustomer)
+                get(CustomerController::getOne)
+                path("list") {
+                    get(CustomerController::listCustomer)
+                }
+            }
+
+
+            //    增加
+            post("/department", DepartmentController::create)
+            //    删除
+            delete("/department", DepartmentController::delete)
+            //    修改
+            put("/department", DepartmentController::update)
+            //    查询
+            get("/department", DepartmentController::getOne)
+            //    查询列表
+            get("/department/list", DepartmentController::list)
+        }
+    }.start(7000)
 
 //    app.error(404) { ctx ->
 //        log.info { "path不存在 : $ctx.req().requestURI" }
@@ -67,44 +100,3 @@ fun main() {
     Runtime.getRuntime().addShutdownHook(Thread { app.stop() })
 
 }
-
-/**
- * 路由，函数式注册
- */
-
-fun routes(app: Javalin) {
-    app.get("/") { ctx ->
-        log.info { "首页访问中" }
-        ctx.result("Hello World，你好世界！—— Javalin for kotlin")
-    }
-    app.get("/param", IndexController::paramReceive)
-    app.post("/user", IndexController::bodyReceive)
-
-
-    // 组合式 Handler groups https://javalin.io/documentation#handler-groups
-    app.routes() {
-        path("customer") {
-            post(CustomerController::createCustomer)
-            delete(CustomerController::deleteCustomer)
-            put(CustomerController::updateCustomer)
-            get(CustomerController::getOne)
-            path("list") {
-                get(CustomerController::listCustomer)
-            }
-        }
-    }
-
-    //    增加
-    app.post("/department", DepartmentController::create)
-    //    删除
-    app.delete("/department", DepartmentController::delete)
-    //    修改
-    app.put("/department", DepartmentController::update)
-    //    查询
-    app.get("/department", DepartmentController::getOne)
-    //    查询列表
-    app.get("/department/list", DepartmentController::list)
-
-
-}
-
