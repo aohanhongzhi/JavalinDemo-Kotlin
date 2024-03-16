337c757829c30b1220fa05f0fe90138d78db35d7
捕风的逍遥侯 <aohanhongzhi@qq.com>
2024-03-16 10:36:15 +0800

    代码定义了一个名为 CustomerService 的对象（object）。在 Kotlin 中，对象（object）可以看作是一个单例对象，具有静态方法的特性。这意味着你可以直接通过 CustomerService.list() 的方式调用该对象的 list 方法。

--- app/src/main/kotlin/hxy/dragon/service/CustomerService.kt
+++ app/src/main/kotlin/hxy/dragon/service/CustomerService.kt
@@ -3,7 +3,7 @@ package hxy.dragon.service
 import hxy.dragon.model.Customer
 import io.ebean.DB
 
-// object 的使用方式很像静态方法，但是实际上应该是直接生成了一个对象，所以可以直接调用里面方法。
+// 代码定义了一个名为 CustomerService 的对象（object）。在 Kotlin 中，对象（object）可以看作是一个单例对象，具有静态方法的特性。这意味着你可以直接通过 CustomerService.list() 的方式调用该对象的 list 方法。
 object CustomerService {
 
     /**
