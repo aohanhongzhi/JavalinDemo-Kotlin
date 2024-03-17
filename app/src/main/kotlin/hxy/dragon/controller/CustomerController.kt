b4d8abeac810df30c3ca585eefd667d1769a1b96
捕风的逍遥侯 <aohanhongzhi@qq.com>
2024-03-17 11:35:36 +0800

    Controller的逻辑转移到了Service里面

--- app/src/main/kotlin/hxy/dragon/controller/CustomerController.kt
+++ app/src/main/kotlin/hxy/dragon/controller/CustomerController.kt
@@ -30,36 +30,19 @@ object CustomerController {
 
     fun getOne(ctx: Context) {
         var id = ctx.queryParamAsClass<Int>("id").get()
-        val customer = DB.find(Customer::class.java).where().eq("id", id).findOne()
-        var obj = BaseResponse(200, "查询到了", customer)
-        if (customer == null) {
-            obj = BaseResponse(404, "没有找到数据", customer)
-        }
+        var obj = CustomerService.getOne(id)
         ctx.json(obj)
     }
 
     fun deleteCustomer(ctx: Context) {
         var id = ctx.queryParamAsClass<Int>("id").get()
-        val delete = DB.find(Customer::class.java).where().eq("id", id).delete()
-        ctx.json(BaseResponse(200, "删除结果", "删除影响行数：" + delete))
+        ctx.json(CustomerService.delete(id))
     }
 
     fun updateCustomer(ctx: Context) {
         val customer = ctx.bodyAsClass(Customer::class.java)
-
-        // 更新方式1
-        customer.update()
-
-        if (false) {
-            // 更新方式2
-            DB.update(customer)
-
-            // 更新方式3：更新指定字段
-            val rows = DB.update(Customer::class.java).set("name", customer.name).where().eq("id", customer.id).update()
-            log.info { "更新影响行数 $rows" }
-        }
-
-        ctx.json(BaseResponse(200, "更新结果", customer))
+        var obj = CustomerService.updateCustomer(customer)
+        ctx.json(obj)
     }
 
 }
--- app/src/main/kotlin/hxy/dragon/service/CustomerService.kt
+++ app/src/main/kotlin/hxy/dragon/service/CustomerService.kt
@@ -1,10 +1,23 @@
 package hxy.dragon.service
 
+import hxy.dragon.entity.BaseResponse
 import hxy.dragon.model.Customer
 import io.ebean.DB
+import io.github.oshai.kotlinlogging.KotlinLogging
+
+private val log = KotlinLogging.logger {}
 
 // 代码定义了一个名为 CustomerService 的对象（object）。在 Kotlin 中，对象（object）可以看作是一个单例对象，具有静态方法的特性。这意味着你可以直接通过 CustomerService.list() 的方式调用该对象的 list 方法。
 object CustomerService {
+
+    fun save() {
+
+    }
+
+    fun delete(id: Int): BaseResponse<String> {
+        val delete = DB.find(Customer::class.java).where().eq("id", id).delete()
+        return BaseResponse(200, "删除结果", "删除影响行数：" + delete)
+    }
 
     /**
      * ebean 查询list
@@ -12,5 +25,29 @@ object CustomerService {
     fun list(): List<Customer> {
         val findList = DB.find(Customer::class.java).findList()
         return findList
+    }
+
+    fun getOne(id: Int): BaseResponse<Customer> {
+        val customer = DB.find(Customer::class.java).where().eq("id", id).findOne()
+        var obj = BaseResponse(200, "查询到了", customer)
+        if (customer == null) {
+            obj = BaseResponse(404, "没有找到数据", customer)
+        }
+        return obj
+    }
+
+    fun updateCustomer(customer: Customer): BaseResponse<Customer> {
+        // 更新方式1
+        customer.update()
+
+        if (false) {
+            // 更新方式2
+            DB.update(customer)
+
+            // 更新方式3：更新指定字段
+            val rows = DB.update(Customer::class.java).set("name", customer.name).where().eq("id", customer.id).update()
+            log.info { "更新影响行数 $rows" }
+        }
+        return BaseResponse(200, "更新结果", customer)
     }
 }
